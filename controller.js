6a1ce8f5e2010083d0815a509dd306b9b40af8ae
Apurv Gunjal <66068557+apurvgunjal@users.noreply.github.com>
2021-06-30 12:21:37 +0530

    Create controller.js

---
+++ controller.js
@@ -0,0 +1,13 @@
+({
+	 doinit : function(component, event, helper) {
+       var action = component.get("c.getOpportunity");        
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                 component.set('v.opps',response.getReturnValue());
+            }
+        });
+        $A.enqueueAction(action); 
+  
+    },
+})
